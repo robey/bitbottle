41887dc6a5206a50b96d527c8f372d0652a41fd8
Robey Pointer <robeypointer@gmail.com>
2015-04-06 08:45:37 -0700

    ported archive!

---
+++ src/4q/lib4q/archive.js
@@ -0,0 +1,227 @@
+"use strict";
+
+const bottle_stream = require("./bottle_stream");
+const events = require("events");
+const file_bottle = require("./file_bottle");
+const fs = require("fs");
+const path = require("path");
+const Promise = require("bluebird");
+const stream = require("stream");
+const toolkit = require("stream-toolkit");
+const util = require("util");
+
+fs.openPromise = Promise.promisify(fs.open);
+fs.readdirPromise = Promise.promisify(fs.readdir);
+fs.statPromise = Promise.promisify(fs.stat);
+
+// higher-level API for maniplating 4Q archives of files & folders.
+
+/*
+ * Create a file or folder bottle stream, emitting events for:
+ *   - "filename", (filename, header) -> begin processing a new file
+ *   - "status", (filename, byteCount) -> current # of bytes read from the current file
+ *   - "error", (error) -> an error occurred during the data streaming
+ */
+class ArchiveWriter extends events.EventEmitter {
+  constructor() {
+    super();
+  }
+
+  /*
+   * Write a file into a bottle and return that a promise for that bottle.
+   * If it's a plain file, the file's contents are piped in.
+   * If it's a folder, a folder bottle is generated, and each file in the
+   * folder is added sequentially. (Nested folders are handled recursively.)
+   * In each case, the promise is fulfilled before the data is completely
+   * written. Handle the "finish" event on the bottle to find out when the
+   * bottle has finished writing.
+   */
+  archiveFile(filename) {
+    return this._processFile(filename, null);
+  }
+
+  /*
+   * Create a fake folder with the given name, and archive a list of files
+   * into it, as with `archiveFile`.
+   */
+  archiveFiles(folderName, filenames) {
+    const header = this._makeFakeFolderHeader(folderName);
+    const prefix = folderName + "/";
+    this.emit("filename", prefix, header);
+    return this._processFolder(null, prefix, header, filenames);
+  }
+
+  _processFile(filename, prefix) {
+    const basename = path.basename(filename);
+    return fs.statPromise(filename).then((stats) => {
+      const header = file_bottle.fileHeaderFromStats(basename, stats);
+      const displayName = (prefix ? path.join(prefix, basename) : basename) + (header.folder ? "/" : "");
+      this.emit("filename", displayName, header);
+      if (header.folder) return this._processFolder(filename, displayName, header);
+      return fs.openPromise(filename, "r").then((fd) => {
+        const countingFileStream = toolkit.countingStream();
+        countingFileStream.on("count", (n) => {
+          this.emit("status", displayName, n);
+        });
+        const fileBottle = new file_bottle.FileBottleWriter(header);
+        fs.createReadStream(filename, { fd }).pipe(countingFileStream).pipe(fileBottle);
+        return fileBottle;
+      });
+    });
+  }
+
+  _processFolder(folderName, prefix, header, files = null) {
+    return (files ? Promise.resolve(files) : fs.readdirPromise(folderName)).then((files) => {
+      const folderBottle = new file_bottle.FolderBottleWriter(header);
+      // fill the bottle in the background, closing it when done.
+      Promise.map(files, (filename) => {
+        const fullPath = folderName ? path.join(folderName, filename) : filename;
+        return this._processFile(fullPath, prefix).then((fileStream) => {
+          return folderBottle.writePromise(fileStream);
+        });
+      }, { concurrency: 1 }).then(() => {
+        return folderBottle.end();
+      }).catch((error) => {
+        this.emit("error", error);
+      });
+      return folderBottle;
+    });
+  }
+
+  _makeFakeFolderHeader(name) {
+    const nowNanos = Date.now() * Math.pow(10, 6);
+    const stats = {
+      folder: true,
+      filename: name,
+      mode: 0x1c0,
+      createdNanos: nowNanos,
+      modifiedNanos: nowNanos,
+      accessedNanos: nowNanos
+    };
+    return stats;
+  }
+}
+
+
+/*
+ * events:
+ * - "start-bottle", (bottle) -> beginning processing of a bottle
+ * - "end-bottle", (bottle) -> done processing a bottle
+ * - "skip", (bottle) -> unknown bottle type, skipping
+ * - "hash", (bottle, isValid, hex) -> after validating a hashed bottle
+ * - "encrypt", (bottle) -> before attempting to decrypt an encrypted bottle
+ * - "compress", (bottle) -> before uncompressing a compressed bottle
+ *
+ * callbacks:
+ * - processFile(dataStream) -> handle contents of a file, return a promise for completion
+ * - decryptKey(keyMap) -> decrypt one of these buffers if possible
+ */
+class ArchiveReader extends events.EventEmitter {
+  constructor() {
+    super();
+  }
+
+  scanStream(inStream) {
+    return bottle_stream.readBottleFromStream(inStream).then((bottle) => {
+      return this.scan(bottle);
+    });
+  }
+
+  scan(bottle) {
+    this.emit("start-bottle", bottle);
+    let promise = null;
+    switch (bottle.type) {
+      case bottle_stream.TYPE_FILE:
+        promise = bottle.header.folder ? this._scanFolder(bottle) : this._scanFile(bottle);
+        break;
+      case bottle_stream.TYPE_HASHED:
+        promise = this._scanHashed(bottle);
+        break;
+      case bottle_stream.TYPE_ENCRYPTED:
+        promise = this._scanEncrypted(bottle);
+        break;
+      case bottle_stream.TYPE_COMPRESSED:
+        promise = this._scanCompressed(bottle);
+        break;
+      default:
+        promise = this._skipBottle(bottle);
+        break;
+    }
+    return promise.then(() => this.emit("end-bottle", bottle));
+  }
+
+  // scan each internal stream recursively
+  _scanFolder(bottle) {
+    return bottle.readPromise().then((nextStream) => {
+      if (nextStream == null) return;
+      return this.scanStream(nextStream).then(() => this._scanFolder(bottle));
+    });
+  }
+
+  _scanFile(bottle) {
+    return bottle.readPromise().then((nextStream) => {
+      if (nextStream == null) return;
+      return this.processFile(nextStream).then(() => this._scanFile(bottle));
+    });
+  }
+
+  // override this method to do something besides just skip the stream and move on.
+  processFile(dataStream) {
+    const sink = toolkit.nullSinkStream();
+    dataStream.pipe(sink);
+    return sink.finishPromise();
+  }
+
+  // given a map of (name -> buffer), where the buffer is an encrypted blob,
+  // try to find a name we recognize and can decrypt, and decrypt the buffer,
+  // returning it as a promise. reject the promise if we can't.
+  decryptKey(keymap) {
+    return Promise.reject(new Error("Encrypted bottle; no keys"));
+  }
+
+  _scanHashed(bottle) {
+    return bottle.validate().then(({ bottle: innerBottle, valid: validPromise, hex: hexPromise }) => {
+      return this.scan(innerBottle).then(() => {
+        return validPromise.then((isValid) => {
+          return hexPromise.then((hex) => {
+            this.emit("hash", bottle, isValid, hex);
+          });
+        });
+      });
+    });
+  }
+
+  _scanEncrypted(bottle) {
+    this.emit("encrypt", bottle);
+    return bottle.readKeys().then((keymap) => {
+      return this.decryptKey(keymap).then((keyBuffer) => {
+        return bottle.decrypt(keyBuffer).then((stream) => {
+          return bottle_stream.readBottleFromStream(stream).then((bottle) => {
+            return this.scan(bottle);
+          });
+        });
+      });
+    });
+  }
+
+  _scanCompressed(bottle) {
+    this.emit("compress", bottle);
+    return bottle.decompress().then((nextBottle) => {
+      return this.scan(nextBottle);
+    });
+  }
+
+  _skipBottle(bottle) {
+    this.emit("skip", bottle);
+    return bottle.readPromise().then((s) => {
+      if (s == null) return;
+      const sink = new toolkit.NullSinkStream();
+      s.pipe(sink);
+      return sink.endPromise().then(() => skipBottle(bottle));
+    });
+  }
+}
+
+
+exports.ArchiveReader = ArchiveReader;
+exports.ArchiveWriter = ArchiveWriter;
---
+++ test/src/test_archive.js
@@ -0,0 +1,110 @@
+"use strict";
+
+const archive = require("../../lib/4q/lib4q/archive");
+const fs = require("fs");
+const mocha_sprinkles = require("mocha-sprinkles");
+const toolkit = require("stream-toolkit");
+const util = require("util");
+
+require("source-map-support").install();
+
+const future = mocha_sprinkles.future;
+const withTempFolder = mocha_sprinkles.withTempFolder;
+
+function archiveWriter() {
+  const w = new archive.ArchiveWriter();
+  w.collectedEvents = [];
+  w.on("filename", (filename, stats) => w.collectedEvents.push({ event: "filename", filename, stats }));
+  w.on("status", (filename, byteCount) => w.collectedEvents.push({ event: "status", filename, byteCount }));
+  return w;
+}
+
+function archiveReader() {
+  const r = new archive.ArchiveReader();
+  r.collectedEvents = [];
+  r.on("start-bottle", (bottle) => r.collectedEvents.push({ event: "start-bottle", bottle }));
+  r.on("end-bottle", (bottle) => r.collectedEvents.push({ event: "end-bottle", bottle }));
+  r.on("hash", (bottle, isValid, hex) => r.collectedEvents.push({ event: "hash-valid", bottle, isValid, hex }));
+  r.processFile = (dataStream) => {
+    return toolkit.pipeToBuffer(dataStream).then((data) => {
+      r.collectedEvents.push({ event: "data", data });
+    });
+  };
+  return r;
+}
+
+
+describe("ArchiveWriter", () => {
+  it("processes a file", future(withTempFolder((folder) => {
+    fs.writeFileSync(`${folder}/test.txt`, "hello");
+    const w = archiveWriter();
+    return w.archiveFile(`${folder}/test.txt`).then((bottle) => {
+      return toolkit.pipeToBuffer(bottle).then((data) => {
+        data.length.should.eql(77);
+        w.collectedEvents.filter((e) => e.event == "filename").map((e) => e.filename).should.eql([ "test.txt" ]);
+      });
+    });
+  })));
+
+  it("processes a folder", future(withTempFolder((folder) => {
+    fs.mkdirSync(`${folder}/stuff`);
+    fs.writeFileSync(`${folder}/stuff/one.txt`, "one!");
+    fs.writeFileSync(`${folder}/stuff/two.txt`, "two!");
+    const w = archiveWriter();
+    return w.archiveFile(`${folder}/stuff`).then((bottle) => {
+      return toolkit.pipeToBuffer(bottle).then((data) => {
+        w.collectedEvents.filter((e) => e.event == "filename").map((e) => e.filename).should.eql([
+          "stuff/",
+          "stuff/one.txt",
+          "stuff/two.txt"
+        ]);
+      });
+    });
+  })));
+});
+
+describe("ArchiveReader", () => {
+  it("reads a file", future(() => {
+    const data = [
+      "f09f8dbc0000003d0008746573742e7478748402a401880800ae4ae2d77e9213",
+      "8c0800ae4ae2d77e9213900800ae4ae2d77e92138001050805726f6265790c05",
+      "776865656c0568656c6c6f00ff"
+    ].join("");
+    const r = archiveReader();
+    return r.scanStream(toolkit.sourceStream(new Buffer(data, "hex"))).then(() => {
+      r.collectedEvents.map((e) => e.event).should.eql([ "start-bottle", "data", "end-bottle" ]);
+      r.collectedEvents[0].bottle.header.filename.should.eql("test.txt");
+      r.collectedEvents[1].data.toString().should.eql("hello");
+    });
+  }));
+
+  it("reads a folder", future(() => {
+    const data = [
+      "f09f8dbc00000039000573747566668402ed0188080040b675ecffa2138c0800",
+      "40b675ecffa21390080040b675ecffa213c0000805726f6265790c0577686565",
+      "6c4bf09f8dbc0000003c00076f6e652e7478748402a40188080040b675ecffa2",
+      "138c080040b675ecffa21390080040b675ecffa2138001040805726f6265790c",
+      "05776865656c046f6e652100ff004bf09f8dbc0000003c000774776f2e747874",
+      "8402a40188080040b675ecffa2138c080040b675ecffa21390080040b675ecff",
+      "a2138001040805726f6265790c05776865656c0474776f2100ff00ff"
+    ].join("");
+    const r = archiveReader();
+    return r.scanStream(toolkit.sourceStream(new Buffer(data, "hex"))).then(() => {
+      r.collectedEvents.map((e) => e.event).should.eql([
+        "start-bottle",
+        "start-bottle",
+        "data",
+        "end-bottle",
+        "start-bottle",
+        "data",
+        "end-bottle",
+        "end-bottle"
+      ]);
+      r.collectedEvents[0].bottle.header.filename.should.eql("stuff");
+      r.collectedEvents[1].bottle.header.filename.should.eql("one.txt");
+      r.collectedEvents[2].data.toString().should.eql("one!");
+      r.collectedEvents[4].bottle.header.filename.should.eql("two.txt");
+      r.collectedEvents[5].data.toString().should.eql("two!");
+    });
+  }));
+});
