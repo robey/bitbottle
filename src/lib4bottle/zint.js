35ae4e3fad9592f22b4f8cf2bee0f3da83072d94
Robey Pointer <robeypointer@gmail.com>
2015-09-27 10:18:38 -0700

    move length-encoding into zint too.

--- src/lib4bottle/zint.js
+++ src/lib4bottle/zint.js
@@ -1,5 +1,11 @@
 "use strict";
 
+/*
+ * methods for encoding ints as:
+ * - packed: LSB, with buffer length passed out-of-band
+ * - length: specialized variable-length encoding that favors powers of two
+ */
+
 export function encodePackedInt(number) {
   if (number < 0) throw new Error("Unsigned ints only, plz");
   const bytes = [];
@@ -21,4 +27,61 @@ export function decodePackedInt(buffer) {
     multiplier *= 256;
   }
   return rv;
+}
+
+/*
+ * 00000000 - end of stream
+ * 0xxxxxxx - 1 thru 2^7 = 128
+ * 10xxxxxx - (+ 1 byte, LSB) = 2^14 = 8K
+ * 110xxxxx - (+ 2 byte, LSB) = 2^21 = 2M
+ * 1110xxxx - (+ 3 byte, LSB) = 2^28 = 128M
+ * 1111xxxx - 2^(7+x) = any power-of-2 block size from 128 to 2^21 = 2M
+ * 11111111 - end of all streams
+ */
+export function encodeLength(n) {
+  if (n < 128) return new Buffer([ n ]);
+  if (n <= Math.pow(2, 22) && (n & (n - 1)) == 0) return new Buffer([ 0xf0 + logBase2(n) - 7 ]);
+  if (n < 8192) return new Buffer([ 0x80 + (n & 0x3f), (n >> 6) & 0xff ]);
+  if (n < Math.pow(2, 21)) return new Buffer([ 0xc0 + (n & 0x1f), (n >> 5) & 0xff, (n >> 13) & 0xff ]);
+  if (n < Math.pow(2, 28)) return new Buffer([ 0xe0 + (n & 0xf), (n >> 4) & 0xff, (n >> 12) & 0xff, (n >> 20) & 0xff ]);
+  throw new Error(`>:-P -> ${n}`);
+}
+
+export function readLength(stream) {
+  return stream.readPromise(1).then(prefix => {
+    if (prefix == null || prefix[0] == 0) return null;
+    if ((prefix[0] & 0x80) == 0) return prefix[0];
+    if ((prefix[0] & 0xf0) == 0xf0) return Math.pow(2, 7 + (prefix[0] & 0xf));
+    if ((prefix[0] & 0xc0) == 0x80) {
+      return stream.readPromise(1).then(data => {
+        if (data == null) return null;
+        return (prefix[0] & 0x3f) + (data[0] << 6);
+      });
+    }
+    if ((prefix[0] & 0xe0) == 0xc0) {
+      return stream.readPromise(2).then(data => {
+        if (data == null) return null;
+        return (prefix[0] & 0x3f) + (data[0] << 5) + (data[1] << 13);
+      });
+    }
+    if ((prefix[0] & 0xf0) == 0xe0) {
+      return stream.readPromise(3).then(data => {
+        if (data == null) return null;
+        return (prefix[0] & 0xf) + (data[0] << 4) + (data[1] << 12) + (data[2] << 20);
+      });
+    }
+    return null;
+  });
+}
+
+// hacker's delight! (only works on exact powers of 2)
+function logBase2(x) {
+  x -= 1;
+  x -= ((x >> 1) & 0x55555555);
+  x = (x & 0x33333333) + ((x >> 2) & 0x33333333);
+  x = (x + (x >> 4)) & 0x0F0F0F0F;
+  x += (x << 8);
+  x += (x << 16);
+  x >>= 24;
+  return x;
 }
--- test/src/test_zint.js
+++ test/src/test_zint.js
@@ -1,12 +1,14 @@
 "use strict";
 
+import * as toolkit from "stream-toolkit";
 import * as zint from "../../lib/lib4bottle/zint";
+import { future } from "mocha-sprinkles";
 
 import "should";
 import "source-map-support/register";
 
 describe("zint", () => {
-  it("encode", () => {
+  it("encode packed", () => {
     zint.encodePackedInt(0).toString("hex").should.eql("00");
     zint.encodePackedInt(100).toString("hex").should.eql("64");
     zint.encodePackedInt(129).toString("hex").should.eql("81");
@@ -15,7 +17,7 @@ describe("zint", () => {
     zint.encodePackedInt(987654321).toString("hex").should.eql("b168de3a");
   });
 
-  it("decode", () => {
+  it("decode packed", () => {
     zint.decodePackedInt(new Buffer("00", "hex")).should.eql(0);
     zint.decodePackedInt(new Buffer("ff", "hex")).should.eql(255);
     zint.decodePackedInt(new Buffer("64", "hex")).should.eql(100);
@@ -24,4 +26,38 @@ describe("zint", () => {
     zint.decodePackedInt(new Buffer("0001", "hex")).should.eql(256);
     zint.decodePackedInt(new Buffer("b168de3a", "hex")).should.eql(987654321);
   });
+
+  it("encode length", () => {
+    zint.encodeLength(1).toString("hex").should.eql("01");
+    zint.encodeLength(100).toString("hex").should.eql("64");
+    zint.encodeLength(129).toString("hex").should.eql("8102");
+    zint.encodeLength(127).toString("hex").should.eql("7f");
+    zint.encodeLength(256).toString("hex").should.eql("f1");
+    zint.encodeLength(1024).toString("hex").should.eql("f3");
+    zint.encodeLength(12345).toString("hex").should.eql("d98101");
+    zint.encodeLength(3998778).toString("hex").should.eql("ea43d003");
+    zint.encodeLength(87654321).toString("hex").should.eql("e1fb9753");
+    zint.encodeLength(Math.pow(2, 21)).toString("hex").should.eql("fe");
+  });
+
+  it("read length", future(() => {
+    const check = (string, expected) => {
+      return zint.readLength(toolkit.sourceStream(new Buffer(string, "hex"))).then(val => {
+        val.should.eql(expected);
+      });
+    };
+
+    return Promise.all([
+      check("01", 1),
+      check("64", 100),
+      check("8102", 129),
+      check("7f", 127),
+      check("f1", 256),
+      check("f3", 1024),
+      check("d98101", 12345),
+      check("ea43d003", 3998778),
+      check("e1fb9753", 87654321),
+      check("fe", Math.pow(2, 21))
+    ]);
+  }));
 });
