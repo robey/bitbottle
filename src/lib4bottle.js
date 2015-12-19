3453a87a36917b9b22f04fed2577276049bdcb2c
Robey Pointer <robeypointer@gmail.com>
2015-12-19 15:08:39 -0500

    clean up exports.

--- src/lib4bottle.js
+++ src/lib4bottle.js
@@ -1,8 +1,39 @@
 "use strict";
 
-export { ArchiveReader, ArchiveWriter, scanArchive } from "./lib4bottle/archive";
-export { BottleWriter, readBottleFromStream, TYPE_FILE, TYPE_HASHED, TYPE_ENCRYPTED, TYPE_COMPRESSED } from "./lib4bottle/bottle_stream";
-export { readCompressedBottle, writeCompressedBottle, COMPRESSION_LZMA2, COMPRESSION_SNAPPY } from "./lib4bottle/compressed_bottle";
-export { encryptedBottleWriter, ENCRYPTION_AES_256_CTR } from "./lib4bottle/encrypted_bottle";
-export { FileBottleWriter, fileHeaderFromStats, FolderBottleWriter } from "./lib4bottle/file_bottle";
-export { validateHashBottle, HashBottleWriter, HASH_SHA512 } from "./lib4bottle/hash_bottle";
+export {
+  ArchiveWriter,
+  scanArchive
+} from "./lib4bottle/archive";
+
+export {
+  BottleWriter,
+  readBottleFromStream,
+  TYPE_FILE,
+  TYPE_HASHED,
+  TYPE_ENCRYPTED,
+  TYPE_COMPRESSED
+} from "./lib4bottle/bottle_stream";
+
+export {
+  FileBottleWriter,
+  fileHeaderFromStats,
+  FolderBottleWriter
+} from "./lib4bottle/file_bottle";
+
+export {
+  validateHashBottle,
+  HashBottleWriter,
+  HASH_SHA512
+} from "./lib4bottle/hash_bottle";
+
+export {
+  encryptedBottleWriter,
+  ENCRYPTION_AES_256_CTR
+} from "./lib4bottle/encrypted_bottle";
+
+export {
+  readCompressedBottle,
+  writeCompressedBottle,
+  COMPRESSION_LZMA2,
+  COMPRESSION_SNAPPY
+} from "./lib4bottle/compressed_bottle";
