09a1a4aedaf7575dbb1fb285f0c124e538c1fd0b
Robey Pointer <robeypointer@gmail.com>
2018-06-05 09:20:49 -0700

    make Readable into a stream, and make unframed push back any buffered data when it finishes.

--- src/framed.ts
+++ src/framed.ts
@@ -1,4 +1,4 @@
-import { Stream } from "ballvalve";
+import { asyncIter, ExtendedAsyncIterator, Stream } from "ballvalve";
 import { debug, nameOf } from "./debug";
 import { Readable } from "./readable";
 import { decodeLength, encodeLength, lengthLength } from "./zint";
@@ -9,21 +9,22 @@ const END_OF_STREAM = Buffer.from([ 0 ]);
  * Prefix each buffer with a length header so it can be streamed. If you want
  * to create large frames, pipe through `buffered` first.
  */
-export async function* framed(stream: Stream): Stream {
-  debug(`framed(${nameOf(stream)}) begin`);
+export async function* _framed(stream: Stream): Stream {
   for await (const data of stream) {
-    debug(`framed(${nameOf(stream)}): ${data.length}`);
     yield encodeLength(data.length);
     yield data;
   }
   yield END_OF_STREAM;
-  debug(`framed(${nameOf(stream)}) end`);
+}
+
+export function framed(stream: Stream): ExtendedAsyncIterator<Buffer> {
+  return asyncIter(_framed(stream));
 }
 
 /*
  * Unpack frames back into data blocks.
  */
-export async function* unframed(stream: Readable): Stream {
+async function* _unframed(stream: Readable): Stream {
   const readLength = async (): Promise<number | undefined> => {
     const byte = await stream.read(1);
     if (byte === undefined || byte.length < 1) return undefined;
@@ -43,4 +44,15 @@ export async function* unframed(stream: Readable): Stream {
     if (data === undefined || data.length < length) throw new Error("Truncated stream");
     yield data;
   }
+}
+
+// take a Readable, pull out a new Readable, and when the new stream ends, push back the remainder.
+export function unframed(stream: Readable): Readable {
+  const iter = asyncIter(_unframed(stream));
+  const r = new Readable(iter);
+  iter.done.then(() => {
+    const remainder = r.remainder();
+    if (remainder) stream.unread(remainder);
+  });
+  return r;
 }
--- src/readable.ts
+++ src/readable.ts
@@ -1,16 +1,18 @@
-import { Stream } from "ballvalve";
+import { ExtendedAsyncIterator, Stream } from "ballvalve";
 
 /*
  * wrap a `Stream` so discrete `read(N)` calls will work.
  */
-export class Readable {
+export class Readable implements Stream, AsyncIterator<Buffer> {
   saved: Buffer[] = [];
   size = 0;
   ended = false;
   iter: AsyncIterator<Buffer>;
+  done: Promise<void>;
 
-  constructor(stream: Stream) {
+  constructor(public stream: ExtendedAsyncIterator<Buffer>) {
     this.iter = stream[Symbol.asyncIterator]();
+    this.done = stream.done;
   }
 
   private async fillTo(size: number): Promise<void> {
@@ -34,8 +36,45 @@ export class Readable {
     // this works even if size > this.size
     const total = Buffer.concat(this.saved);
     const rv = total.slice(0, size);
-    this.saved = [ total.slice(size) ];
-    this.size = this.saved[0].length;
+    if (size == total.length) {
+      this.saved = [];
+      this.size = 0;
+    } else {
+      this.saved = [ total.slice(size) ];
+      this.size = this.saved[0].length;
+    }
     return rv;
+  }
+
+  unread(b: Buffer) {
+    this.saved.unshift(b);
+    this.size += b.length;
+  }
+
+  [Symbol.asyncIterator]() {
+    return this;
+  }
+
+  async next(): Promise<IteratorResult<Buffer>> {
+    const b = this.saved.shift();
+    if (b !== undefined) {
+      this.size -= b.length;
+      return { done: false, value: b };
+    }
+    const rv = await this.iter.next();
+    if (rv.done) this.ended = true;
+    return rv;
+  }
+
+  remainder(): Buffer | undefined {
+    if (this.size == 0) return undefined;
+    const rv = Buffer.concat(this.saved);
+    this.saved = [];
+    this.size = 0;
+    return rv;
+  }
+
+  toString(): string {
+    return `Readable(${this.stream})`;
   }
 }
