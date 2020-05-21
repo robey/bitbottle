de667e42c872a8fac14825e5b81c1c9412a41a2e
Robey Pointer <robeypointer@gmail.com>
2020-05-21 12:23:43 -0700

    can now write a bottle

--- src/bottle.ts
+++ src/bottle.ts
@@ -1,150 +1,41 @@
-import { Decorate, Stream } from "ballvalve";
-import { Crc32 } from "./crc32";
-import { framed, unframed } from "./framed";
-import { Header } from "./header";
-import { Readable } from "./readable";
+import { asyncIter } from "ballvalve";
+import { BottleCap } from "./bottle_cap";
+import { framed } from "./framed";
 
-export const MAGIC = Buffer.from([ 0xf0, 0x9f, 0x8d, 0xbc ]);
-export const VERSION = 0x00;
+// frame headers 40, 80, c0 are reserved, so use them for out-of-band signalling.
+const STREAM_RAW = 0x40;
+const STREAM_BOTTLE = 0x80;
+const STREAM_END = 0xc0;
 
 let counter = 0;
 
 
-export enum BottleType {
-  File = 0,
-  Hashed = 1,
-  Encrypted = 3,
-  Compressed = 4,
-}
-
-
 export class Bottle {
-  private constructor(public cap: BottleCap, public streams: AsyncIterator<Stream>) {
+  constructor(public cap: BottleCap, public streams: AsyncIterable<AsyncIterable<Buffer> | Bottle>) {
     // pass
   }
 
-  async nextStream(): Promise<Stream> {
-    const item = await this.streams.next();
-    if (item.done) throw new Error(`Missing stream in ${this.cap}`);
-    return item.value;
-  }
-
-  async assertEndOfStreams(): Promise<void> {
-    const item = await this.streams.next();
-    if (!item.done) throw new Error(`Extra stream in ${this.cap}`);
-  }
-
-  // extract the first stream, and throw an error if there were any more
-  // after. has the nice side effect of not closing the new stream until the
-  // underlying readable is exhausted.
-  onlyOneStream(): Stream {
+  write(): AsyncIterable<Buffer> {
+    const id = ++counter;
     const self = this;
 
-    return Decorate.asyncIterator(
-      async function* (): Stream {
-        for await (const buffer of Decorate.asyncIterator(await self.nextStream())) yield buffer;
-        self.assertEndOfStreams();
-      }(),
-      () => self.cap.toString()
-    );
-  }
-
-  static write(type: BottleType, header: Header, streams: AsyncIterator<Stream>): Stream {
-    const id = ++counter;
-    const cap = new BottleCap(type, header);
-
-    return Decorate.asyncIterator(
-      async function* (): Stream {
-        yield cap.write();
-        for await (const s of Decorate.asyncIterator(streams)) {
-          for await (const buffer of Decorate.asyncIterator(framed(s))) yield buffer;
-        }
-      }(),
-      () => `BottleWriter[${id}](${cap.toString()})`
-    );
-  }
-
-  static async read(readable: Readable): Promise<Bottle> {
-    const id = ++counter;
-    const cap = await BottleCap.read(readable);
-    return new Bottle(
-      cap,
-      Decorate.asyncIterator(
-        async function* () {
-          while (true) {
-            const byte = await readable.read(1);
-            if (byte === undefined || byte.length < 1) {
-              // we hit the end.
-              return;
-            }
-            readable.unread(byte);
-
-            const outStream = Decorate.asyncIterator(unframed(readable));
-            yield outStream;
-            await outStream.onEnd();
+    return asyncIter(
+      async function* (): AsyncIterable<Buffer> {
+        yield self.cap.write();
+        for await (const s of self.streams) {
+          if (s instanceof Bottle) {
+            yield Buffer.from([ STREAM_BOTTLE ]);
+            yield* s.write();
+          } else {
+            // raw stream
+            yield Buffer.from([ STREAM_RAW ]);
+            yield* framed(s);
           }
-        }(),
-        () => `BottleReader[${id}](${cap}, ${readable})`
-      )
+        }
+        yield Buffer.from([ STREAM_END ]);
+      }(),
+      () => `BottleWriter[${id}](${self.cap.toString()})`
     );
   }
-}
 
-
-export class BottleCap {
-  constructor(public type: BottleType, public header: Header) {
-    // pass
-  }
-
-  toString(): string {
-    return `Bottle(${this.type}, ${this.header})`;
-  }
-
-  write(): Buffer {
-    if (this.type < 0 || this.type > 15) throw new Error(`Bottle type out of range: ${this.type}`);
-    const buffer = this.header.pack();
-    if (buffer.length > 4095) throw new Error(`Header too long: ${buffer.length} > 4095`);
-
-    const cap = Buffer.concat([
-      MAGIC,
-      Buffer.from([
-        VERSION,
-        0,
-        (buffer.length & 0xff),
-        (this.type << 4) | ((buffer.length >> 8) & 0xf),
-      ]),
-      buffer
-    ]);
-
-    return Buffer.concat([ cap, Crc32.lsbFrom(cap) ]);
-  }
-
-  static async read(stream: Readable): Promise<BottleCap> {
-    const crc = new Crc32();
-
-    const b = await stream.read(8);
-    if (b === undefined || b.length < 8) throw new Error("End of stream");
-    if (!b.slice(0, 4).equals(MAGIC)) throw new Error("Incorrect magic (not a bitbottle)");
-    crc.update(b);
-
-    const version = b[4];
-    const flags = b[5];
-    const headerLength = b[6] + (b[7] & 0xf) * 256;
-    const type = b[7] >> 4;
-    if ((version >> 4) > 0) throw new Error(`Incompatible version: ${version >> 4}.${version & 0xf}`);
-    if (flags != 0) throw new Error(`Garbage flags`);
-
-    let header = new Header();
-    if (headerLength > 0) {
-      const b2 = await stream.read(headerLength);
-      if (b2 === undefined || b2.length < headerLength) throw new Error("Truncated header");
-      header = Header.unpack(b2);
-      crc.update(b2);
-    }
-
-    const encodedCrc = await stream.read(4);
-    if (encodedCrc === undefined || encodedCrc.length < 4) throw new Error("Truncated header");
-    if (encodedCrc.readUInt32LE(0) != crc.finish()) throw new Error("CRC-32 mismatch in header");
-    return new BottleCap(type, header);
-  }
 }
--- src/test/test_bottle.ts
+++ src/test/test_bottle.ts
@@ -1,147 +1,46 @@
-import { Decorate, Stream } from "ballvalve";
+import { asyncIter, byteReader } from "ballvalve";
 import { Bottle } from "../bottle";
+import { BottleCap, MAGIC } from "../bottle_cap";
 import { Header } from "../header";
-import { Readable } from "../readable";
-import { delay, hex } from "./tools";
+import { fromHex, hex } from "./tools";
 
 import "should";
 import "source-map-support/register";
 
-const MAGIC_STRING = "f09f8dbc0000";
-const BASIC_MAGIC = MAGIC_STRING + "00e09dcdda54";
+const MAGIC_STRING = "f09f8dbc00";
+
+const CAP_10 = new BottleCap(10, new Header());
+const CAP_10_HEX = `${MAGIC_STRING}0a000033aa47f9`;
+const CAP_14 = new BottleCap(14, new Header());
+const CAP_14_HEX = `${MAGIC_STRING}0e0000ef024efe`;
+
+function read(hex: string): Promise<BottleCap> {
+  return BottleCap.read(byteReader([ Buffer.from(hex, "hex") ]));
+}
 
 
 describe("Bottle.write", () => {
   it("writes a bottle header", async () => {
-    const b = Bottle.write(10, new Header().addInt(0, 150), Decorate.iterator([]));
-    (await hex(b)).should.eql(`${MAGIC_STRING}03a0018096cc8641ed`);
+    const cap = new BottleCap(10, new Header().addU8(0, 150));
+    const b = new Bottle(cap, asyncIter([]));
+    (await hex(b.write())).should.eql(`${MAGIC_STRING}0a02001096d528167ac0`);
   });
 
   it("writes data", async () => {
-    const data = Decorate.iterator([ Buffer.from("ff00ff00", "hex") ]);
-    const b = Bottle.write(10, new Header(), Decorate.iterator([ data ]));
-    (await hex(b)).should.eql(`${MAGIC_STRING}00a00d8c062204ff00ff0000`);
+    const b = new Bottle(CAP_10, asyncIter([ fromHex("ff00ff00") ]));
+    (await hex(b.write())).should.eql(`${CAP_10_HEX}4004ff00ff00c0`);
   });
 
   it("writes a nested bottle", async () => {
-    const b2 = Bottle.write(14, new Header(), Decorate.iterator([]));
-    const b = Bottle.write(10, new Header(), Decorate.iterator([ b2 ]));
-    (await hex(b)).should.eql(`${MAGIC_STRING}00a00d8c06220c${MAGIC_STRING}00e09dcdda5400`);
+    const b2 = new Bottle(CAP_14, asyncIter([]));
+    const b = new Bottle(CAP_10, asyncIter([ b2 ]));
+    (await hex(b.write())).should.eql(`${CAP_10_HEX}80${CAP_14_HEX}c0c0`);
   });
 
   it("writes a nested bottle of data", async () => {
-    const data = Decorate.iterator([ Buffer.from("cat") ]);
-    const b2 = Bottle.write(14, new Header(), Decorate.iterator([ data ]));
-    const b = Bottle.write(10, new Header(), Decorate.iterator([ b2 ]));
-
-    const nested = `0c${MAGIC_STRING}00e09dcdda54010303636174010000`;
-    (await hex(b)).should.eql(`${MAGIC_STRING}00a00d8c0622${nested}`);
+    const b2 = new Bottle(CAP_14, asyncIter([ fromHex("636174") ]));
+    const b = new Bottle(CAP_10, asyncIter([ b2 ]));
+    (await hex(b.write())).should.eql(`${CAP_10_HEX}80${CAP_14_HEX}4003636174c0c0`);
   });
 
-  it("streams data", async () => {
-    // just to verify that the data is written as it comes in, and the event isn't triggered until completion.
-    let done = false;
-    async function* data(): Stream {
-      await delay(10);
-      yield Buffer.from("c44c", "hex");
-      await delay(10);
-      done = true;
-    }
-
-    const b = Bottle.write(14, new Header(), Decorate.iterator([ data() ]));
-    done.should.eql(false);
-    (await hex(b)).should.eql(`${MAGIC_STRING}00e09dcdda5402c44c00`);
-    done.should.eql(true);
-  });
-
-  it("writes several datas", async () => {
-    const data1 = Decorate.iterator([ Buffer.from("f0f0f0", "hex") ]);
-    const data2 = Decorate.iterator([ Buffer.from("e0e0e0", "hex") ]);
-    const data3 = Decorate.iterator([ Buffer.from("cccccc", "hex") ]);
-
-    async function* streams(): AsyncIterator<Stream> {
-      await delay(10);
-      yield data1;
-      await delay(10);
-      yield data2;
-      await delay(10);
-      yield data3;
-    }
-
-    const b = Bottle.write(14, new Header(), streams());
-    (await hex(b)).should.eql(`${MAGIC_STRING}00e09dcdda5403f0f0f00003e0e0e00003cccccc00`);
-  });
-});
-
-
-describe("bottleReader", () => {
-  function read(hex: string): Promise<Bottle> {
-    return Bottle.read(new Readable(Decorate.iterator([ Buffer.from(hex, "hex") ])));
-  }
-
-  it("validates the header", async () => {
-    await read("00").should.be.rejectedWith(/End of stream/);
-    await read("00ff00ff00ff00ffcccccccc").should.be.rejectedWith(/magic/);
-    await read("f09f8dbcff000000cccccccc").should.be.rejectedWith(/version/);
-    await read("f09f8dbc00ff0000cccccccc").should.be.rejectedWith(/flags/);
-    await read("f09f8dbc000000c0cccccccc").should.be.rejectedWith(/CRC/);
-  });
-
-  it("reads the header", async () => {
-    const b = await read("f09f8dbc000000c055edb46f");
-    b.cap.type.should.eql(12);
-    b.cap.header.toString().should.eql("Header()");
-
-    const b2 = await read("f09f8dbc000003e0018096f1de5576");
-    b2.cap.type.should.eql(14);
-    b2.cap.header.toString().should.eql("Header(I0=150)");
-  });
-
-  it("reads a data block", async () => {
-    const b = await read(`${BASIC_MAGIC}0568656c6c6f00`);
-    const stream1 = (await b.streams.next()).value;
-    Buffer.concat(await Decorate.asyncIterator(stream1).collect()).toString().should.eql("hello");
-    (await b.streams.next()).done.should.eql(true);
-  });
-
-  it("reads a continuing data block", async () => {
-    const b = await read(`${BASIC_MAGIC}026865016c026c6f00`);
-    const stream1 = (await b.streams.next()).value;
-    Buffer.concat(await Decorate.asyncIterator(stream1).collect()).toString().should.eql("hello");
-    (await b.streams.next()).done.should.eql(true);
-  });
-
-  it("reads several datas", async () => {
-    const b = await read(`${BASIC_MAGIC}03f0f0f00003e0e0e00003cccccc00`);
-
-    const stream1 = (await b.streams.next()).value;
-    Buffer.concat(await Decorate.asyncIterator(stream1).collect()).toString("hex").should.eql("f0f0f0");
-
-    const stream2 = (await b.streams.next()).value;
-    Buffer.concat(await Decorate.asyncIterator(stream2).collect()).toString("hex").should.eql("e0e0e0");
-
-    const stream3 = (await b.streams.next()).value;
-    Buffer.concat(await Decorate.asyncIterator(stream3).collect()).toString("hex").should.eql("cccccc");
-
-    (await b.streams.next()).done.should.eql(true);
-  });
-
-  it("reads nested bottles", async () => {
-    const nested = `0c${MAGIC_STRING}00b0699cb13f05036361740000`;
-    const b = await read(`${MAGIC_STRING}00a00d8c0622${nested}0362617400`);
-    b.cap.type.should.eql(10);
-
-    const stream1 = (await b.streams.next()).value;
-    const b2 = await Bottle.read(new Readable(stream1));
-    b2.cap.type.should.eql(11);
-
-    const innerStream = (await b2.streams.next()).value;
-    Buffer.concat(await Decorate.asyncIterator(innerStream).collect()).toString().should.eql("cat");
-
-    (await b2.streams.next()).done.should.eql(true);
-
-    const stream2 = (await b.streams.next()).value;
-    Buffer.concat(await Decorate.asyncIterator(stream2).collect()).toString().should.eql("bat");
-    (await b.streams.next()).done.should.eql(true);
-  });
 });
