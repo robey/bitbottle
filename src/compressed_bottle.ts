14ff49946445291d1e9de8afd65baacfc290c762
Robey Pointer <robeypointer@gmail.com>
2020-07-30 18:11:17 -0700

    fix snappy: we need to encode frame data because the compressed block may be smaller than the uncompressed (of course)

--- src/compressed_bottle.ts
+++ src/compressed_bottle.ts
@@ -4,6 +4,7 @@ import * as xz from "xz";
 import { asyncOne } from "./async";
 import { Bottle } from "./bottle";
 import { BottleCap, BottleType } from "./bottle_cap";
+import { framed, unframed } from "./framed";
 import { Header } from "./header";
 
 const LZMA2_PRESET = 9;
@@ -22,7 +23,7 @@ export interface CompressionOptions {
   compression?: Compression;
 
   // how many bytes to pack into each block, with snappy
-  blockSize?: number;
+  snappyBlockSize?: number;
 }
 
 export interface UncompressedBottle {
@@ -37,13 +38,17 @@ const MAX_BLOCK_SIZE = 1024 * 1024;  // 1MB
 
 
 // blockSize only matters for snappy
-export async function writeCompressedBottle(bottle: AsyncIterator<Buffer>, options: CompressionOptions = {}): Promise<Bottle> {
+export async function writeCompressedBottle(
+  bottle: AsyncIterator<Buffer>,
+  options: CompressionOptions = {}
+): Promise<Bottle> {
   const method = options.compression ?? Compression.SNAPPY;
-  if (options.blockSize !== undefined && (options.blockSize < MIN_BLOCK_SIZE || options.blockSize > MAX_BLOCK_SIZE)) {
+  let blockSize = options.snappyBlockSize ?? DEFAULT_BLOCK_SIZE;
+  if (blockSize < MIN_BLOCK_SIZE || blockSize > MAX_BLOCK_SIZE) {
     throw new Error("Invalid block size");
   }
-  const blockSizeBits = Math.round(Math.log2(options.blockSize ?? DEFAULT_BLOCK_SIZE));
-  const blockSize = Math.pow(2, blockSizeBits);
+  const blockSizeBits = Math.round(Math.log2(blockSize));
+  blockSize = Math.pow(2, blockSizeBits);
 
   const header = new Header();
   header.addInt(Field.IntCompressionType, method);
@@ -109,7 +114,10 @@ async function* compressSnappy(stream: AsyncIterator<Buffer>, blockSize: number)
   while (true) {
     const buffer = await reader.read(blockSize);
     if (buffer === undefined) return;
-    yield snappy.compressSync(buffer);
+    // must frame, because snappy needs exact frames
+    const compressed = snappy.compressSync(buffer);
+    const compressedFramed = Buffer.concat(await asyncIter(framed(asyncOne(compressed))).collect());
+    yield compressedFramed;
   }
 }
 
@@ -117,8 +125,13 @@ async function* uncompressSnappy(stream: AsyncIterator<Buffer>, blockSize: numbe
   const reader = byteReader(stream);
 
   while (true) {
-    const buffer = await reader.read(blockSize);
-    if (buffer === undefined) return;
-    yield snappy.uncompressSync(buffer, { asBuffer: true }) as Buffer;
+    let chunk: Buffer;
+    try {
+      chunk = Buffer.concat(await unframed(reader).collect());
+    } catch (error) {
+      // end of stream
+      return;
+    }
+    yield snappy.uncompressSync(chunk, { asBuffer: true }) as Buffer;
   }
 }
--- src/test/test_compressed_bottle.ts
+++ src/test/test_compressed_bottle.ts
@@ -4,7 +4,7 @@ import { Bottle } from "../bottle";
 import { BottleCap, BottleType } from "../bottle_cap";
 import { Compression, readCompressedBottle, writeCompressedBottle } from "../compressed_bottle";
 import { Header } from "../header";
-import { drain } from "./tools";
+import { drain, prngBytes } from "./tools";
 
 const TEST_STRING = "My cat's breath smells like cat food.";
 const CAP_14 = new BottleCap(14, new Header());
@@ -38,5 +38,24 @@ describe("CompressedBottle", () => {
     b2.cap.type.should.eql(14);
     b2.cap.header.toString().should.eql("Header()");
     (await drain(await b2.nextDataStream())).toString().should.eql(TEST_STRING);
+  });
+
+  it("compresses multiple blocks with snappy", async () => {
+    const LARGE = prngBytes(Math.pow(2, 20));
+    const clearBottle = new Bottle(CAP_14, asyncify([ asyncOne(LARGE) ]));
+    const bottle = await writeCompressedBottle(clearBottle.write(), {
+      compression: Compression.SNAPPY,
+      snappyBlockSize: 16384,
+    });
+    const data = await drain(bottle.write());
+
+    const b = await Bottle.read(byteReader(asyncify([ data ])));
+    b.cap.type.should.eql(BottleType.COMPRESSED);
+    b.cap.header.toString().should.eql("Header(U8(0)=1, U8(1)=14)");
+
+    const b2 = (await readCompressedBottle(b)).bottle;
+    b2.cap.type.should.eql(14);
+    b2.cap.header.toString().should.eql("Header()");
+    (await drain(await b2.nextDataStream())).should.eql(LARGE);
   });
 });
